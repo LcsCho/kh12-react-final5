2b9504c5623456cfcc47cb39e5372f4b1f40cfba
LcsCho <chojunhee98@gmail.com>
2023-11-08 23:47:52 +0900

    메인페이지 꾸미기

--- final/src/App.js
+++ final/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import Menu from './components/Menu';
-import { AiOutlinePlus } from "react-icons/ai";
+
+import MovieRegister from './components/MovieRegister';
+import Home from './components/Home';
+import MovieList from './components/MovieList';
+import MemberList from './components/MemberList';
+import { NavLink, Routes, Route } from 'react-router-dom';
 
 function App() {
-  const iconStyle = {
-    color: 'black',
-    opacity: '100%',
-  };
-
-  const buttonStyle = {
-    backgroundColor: '#B33939',
-    opacity: '30%',
-  };
-
-  const spanStyle = {
-    color: 'black',
-    opacity: '100%', // span 요소의 opacity를 100%로 설정
-  };
+ 
 
   return (
     <div className="container-fluid my-5 py-5">
@@ -27,43 +18,18 @@ function App() {
       {/* 본문 영역 */}
       <div className="row">
         <div className="col-md-8 offset-md-2 col-sm-10 offset-sm-1">
-          <div style={{ border: '2px solid #B33939', borderRadius: '5px' }} className="p-4">
-            <h1>관리자</h1>
-            <hr />
-            <div className="row">
-              <div className="col-md-4">
-                <button className="btn btn-block" style={buttonStyle}>
-                  <AiOutlinePlus style={iconStyle} /> <span className="text-black" style={spanStyle}>영화 등록</span>
-                </button>
-              </div>
-              <div className="col-md-4">
-                <button className="btn btn-block" style={buttonStyle}>
-                  영화 목록
-                </button>
-              </div>
-              <div className="col-md-4">
-                <button className="btn btn-block" style={buttonStyle}>
-                  회원 목록
-                </button>
-              </div>
-            </div>
-            <hr />
-            <div className="row">
-              <div className="col-md-4">
-                <p>회원 수</p>
-              </div>
-              <div className="col-md-4">
-                <p>영화 개수</p>
-              </div>
-              <div className="col-md-4">
-                <p>평점 수</p>
-              </div>
-            </div>
-            <hr />
-          </div>
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route path="/movieRegister" element={<MovieRegister />}></Route>
+            <Route path="/movieList" element={<MovieList />}></Route>
+            <Route path="/memberList" element={<MemberList />}></Route>
+          </Routes>
         </div>
       </div>
-    </div>
+   
+        
+        
+      </div>
   );
 }
 
---
+++ final/src/components/Home.js
@@ -0,0 +1,69 @@
+import { AiOutlinePlus, AiOutlineUnorderedList } from "react-icons/ai";
+import { NavLink, useLocation } from "react-router-dom";
+
+const Home = (props) => {
+  const location = useLocation();
+  const iconStyle = {
+    color: "#B33939",
+    opacity: "100%",
+  };
+  const navLinkStyle = {
+    backgroundColor: "#B33939",
+    opacity: "30%",
+  };
+  const spanStyle = {
+    color: "black",
+    opacity: "100%",
+  };
+
+  const IconNavLink = ({ icon, to, label }) => {
+    return (
+      <NavLink
+        style={navLinkStyle}
+        className={`nav-link ${location.pathname === to ? "active" : ""}`}
+        to={to}
+        display="inline-block"
+      >
+        {icon}
+        <span style={spanStyle}>{label}</span>
+      </NavLink>
+    );
+  };
+
+  return (
+    <div style={{ marginTop: '100px', marginBottom: '100px' }}>
+        <div className="col-md-10 offset-md-1 col-sm-10 offset-sm-1 mt-5">
+            <div style={{ border: '2px solid #B33939', borderRadius: '5px', marginTop: '20px', marginBottom: '20px', width: "100%", height: "400px" }} className="p-4">
+                <h1>관리자</h1>
+                <hr />
+                <div className="row">
+                    <div className="col-md-4">
+                        <IconNavLink icon={<AiOutlinePlus style={iconStyle} />} to="/movieRegister" label="영화 등록" />
+                    </div>
+                    <div className="col-md-4">
+                        <IconNavLink icon={<AiOutlineUnorderedList style={iconStyle} />} to="/movieList" label="영화 목록" />
+                    </div>
+                    <div className="col-md-4">
+                        <IconNavLink icon={<AiOutlineUnorderedList style={iconStyle} />} to="/memberList" label="회원 목록" />
+                    </div>
+                </div>
+                <hr />
+                <div className="row">
+                    <div className="col-md-4">
+                        <p>회원 수</p>
+                    </div>
+                    <div className="col-md-4">
+                        <p>영화 개수</p>
+                    </div>
+                    <div className="col-md-4">
+                        <p>평점 수</p>
+                    </div>
+                </div>
+                <hr />
+            </div>
+        </div>
+    </div>
+);
+};
+
+export default Home;
